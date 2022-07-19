d2c4e68ea2eb7cdc7a720e3b7b5298f9f232b5ae
Salverte <silvina.alverte@gmail.com>
2022-07-19 17:16:16 -0300

    feat: Html y CSS general

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+//tree shaking
 import { Component } from '@angular/core';
 
 @Component({
+  // selector = etiqueta del html
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'petfriendlymdz';
+  title = 'Bienvenidos a mi Proyecto';
 }

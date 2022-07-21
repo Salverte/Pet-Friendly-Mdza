f2699022f52e8eda773cf109821780c7575a8ea2
Salverte <silvina.alverte@gmail.com>
2022-07-21 14:13:57 -0300

    feat: creación cardwrapper y nav responsive 767px

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { CardComponent } from './components/card/card.component';
+import { CardwrapperComponent } from './components/cardwrapper/cardwrapper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    CardComponent
+    CardComponent,
+    CardwrapperComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/cardwrapper/cardwrapper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cardwrapper',
+  templateUrl: './cardwrapper.component.html',
+  styleUrls: ['./cardwrapper.component.scss']
+})
+export class CardwrapperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

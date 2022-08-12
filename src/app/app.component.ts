00ea2d28eb9ad98254efdfb6a570cb8c89062d46
Salverte <silvina.alverte@gmail.com>
2022-08-11 22:13:07 -0300

    Fix: Home tipeo y Css.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Bienvenidos a mi Pet Friendly Mendoza🐾';
+  title = 'Bienvenidos a Pet Friendly Mendoza🐾';
 }
